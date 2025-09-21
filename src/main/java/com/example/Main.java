525a3cac66fe6fe55b554376519d73f4ab2f2fbb
EraiicPhu <eraiicphu@gmail.com>
2025-09-21 16:54:14 +0200

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,11 @@ import com.example.api.ElpriserAPI;
 
 public class Main {
     public static void main(String[] args) {
+
+
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+
+
+        System.out.println(elpriserAPI.getPriser("2025-09-21", ElpriserAPI.Prisklass.SE3));
     }
 }
