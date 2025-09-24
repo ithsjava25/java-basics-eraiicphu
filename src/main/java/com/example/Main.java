0ebda325d704f193f263e27e7c1fe2c583298701
EraiicPhu <eraiicphu@gmail.com>
2025-09-24 23:21:10 +0200

    Update Main.java
    working progress
    
    fixed functionality for downloading prices for current/next day
    + mean price for current day

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,14 +1,47 @@
 package com.example;
 
 import com.example.api.ElpriserAPI;
+import com.example.api.ElpriserAPI.Prisklass;
+import com.example.api.ElpriserAPI.Elpris;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
         ElpriserAPI elpriserAPI = new ElpriserAPI();
 
+        String askedDatumInStringFormat = "2025-09-25";
 
-        System.out.println(elpriserAPI.getPriser("2025-09-21", ElpriserAPI.Prisklass.SE3));
+        LocalDate askedDatumInLocalDate = LocalDate.parse(askedDatumInStringFormat, DateTimeFormatter.ISO_LOCAL_DATE);
+
+        LocalDate tomorrowOfAskedDatum =  askedDatumInLocalDate.plusDays(1);
+
+        Prisklass priceClass = Prisklass.SE3;
+
+        List<Elpris> mergedList = elpriserAPI.getPriser(askedDatumInLocalDate, priceClass);
+        mergedList.addAll(elpriserAPI.getPriser(tomorrowOfAskedDatum, priceClass));
+
+        List<Elpris> priceForAskedDate = elpriserAPI.getPriser(askedDatumInLocalDate, priceClass);
+
+        double meanPriceChosenDate = 0;
+
+
+        //for each loop
+        for (Elpris elpris : priceForAskedDate) {
+            meanPriceChosenDate += elpris.sekPerKWh();
+        }
+        meanPriceChosenDate = meanPriceChosenDate/24;
+        System.out.println("Mean price for chosen date is: " + meanPriceChosenDate);
+
+
+
+        // 1. Kunna välja mitt datum som jag ska hämta elpriser för.
+        // 2. Kunna välja för vilken zon som jag ska hämta elprise för.
+        System.out.println("Test Elpris data som är hämtad för efterfrågade datumet: "
+                + mergedList);
+
     }
 }
