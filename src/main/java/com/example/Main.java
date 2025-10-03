4cd367085b67e403a0fe425e816a144ef24e3c06
EraiicPhu <eraiicphu@gmail.com>
2025-10-04 01:41:40 +0200

    Wip added cheapest and most expensive price

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,28 +5,29 @@ import com.example.api.ElpriserAPI.Prisklass;
 import com.example.api.ElpriserAPI.Elpris;
 
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
 
-        String askedDatumInStringFormat = "2025-09-25";
+        String askedDatumInStringFormat = "2025-09-28";
+        Prisklass priceClass = Prisklass.SE3;
+        double meanPriceChosenDate = 0;
 
         LocalDate askedDatumInLocalDate = LocalDate.parse(askedDatumInStringFormat, DateTimeFormatter.ISO_LOCAL_DATE);
 
         LocalDate tomorrowOfAskedDatum =  askedDatumInLocalDate.plusDays(1);
 
-        Prisklass priceClass = Prisklass.SE3;
-
         List<Elpris> mergedList = elpriserAPI.getPriser(askedDatumInLocalDate, priceClass);
         mergedList.addAll(elpriserAPI.getPriser(tomorrowOfAskedDatum, priceClass));
 
         List<Elpris> priceForAskedDate = elpriserAPI.getPriser(askedDatumInLocalDate, priceClass);
-
-        double meanPriceChosenDate = 0;
 
 
         //for each loop
@@ -36,12 +37,33 @@ public class Main {
         meanPriceChosenDate = meanPriceChosenDate/24;
         System.out.println("Mean price for chosen date is: " + meanPriceChosenDate);
 
+        double cheapestDailyPrice = Double.MAX_VALUE;
+
+        double mostExpensiveDailyPrice = Double.MIN_VALUE;
+
+        for (Elpris elpris : mergedList) {
+            double price = elpris.sekPerKWh();
+            ZonedDateTime cheapestHourStart = elpris.timeStart();
+            ZonedDateTime mostExpensiveHourStart = elpris.timeStart();
+
+            if (price < cheapestDailyPrice){
+                cheapestDailyPrice = price;
+                cheapestHourStart = elpris.timeStart();
+            }
+            if (price > mostExpensiveDailyPrice){
+                mostExpensiveDailyPrice = price;
+                mostExpensiveHourStart = elpris.timeStart();
+            }
+
+        }
+        System.out.println("Printing merged list" + mergedList);
+        System.out.println(cheapestDailyPrice);
+        System.out.println(mostExpensiveDailyPrice);
+        System.out.println("cheapest hour: " + cheapestDailyPrice + " most expensive hour: " + mostExpensiveDailyPrice);
 
 
-        // 1. Kunna välja mitt datum som jag ska hämta elpriser för.
-        // 2. Kunna välja för vilken zon som jag ska hämta elprise för.
-        System.out.println("Test Elpris data som är hämtad för efterfrågade datumet: "
-                + mergedList);
+
+
 
     }
 }
